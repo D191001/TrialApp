3b22723aed53571314b0b2837e5798b3b05e4db5
DmitryK <infosecoff@yandex.com>
2025-04-22 00:52:49 +0300

    test cicd3

--- backend/main.py
+++ backend/main.py
@@ -28,6 +28,7 @@ app.include_router(feedback.router, prefix="/feedback", tags=["feedback"])
 
 
 @app.get("/health-check")
+@app.get("/api/health-check")
 async def health_check():
     return {"status": "healthy"}
 
