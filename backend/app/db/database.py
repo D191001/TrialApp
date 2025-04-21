24048abf2aa89c7c9845d82ffe96529caf8e6496
DmitryK <infosecoff@yandex.com>
2025-04-21 23:43:54 +0300

    test cicd2

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,11 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL", "postgresql://django_user:mysecretpassword@db:5432/django"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
+engine = create_engine(
+    DATABASE_URL, echo=True, pool_pre_ping=True  # Для отладки
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
